ff24b8e512e1aba88641a75fba33403726d140b4
Mavlonberdiyev Xudoyberdi <129485330+urgut200404@users.noreply.github.com>
2025-01-09 12:56:00 +0300

    Update vite.config.js
    asa

--- vite.config.js
+++ vite.config.js
@@ -37,16 +37,34 @@
 
 
 
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+// })
+
+
+
+
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@canvasjs/vue-charts': 'node_modules/@canvasjs/vue-charts'
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['@canvasjs/vue-charts'],
+    },
+  },
 })
-
-
-
 
 
 
