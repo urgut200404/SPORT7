ce8280cb0a8c65901b7e240e214716ea7cdc5fd1
Mavlonberdiyev Xudoyberdi <129485330+urgut200404@users.noreply.github.com>
2025-01-09 19:03:54 +0300

    Update vite.config.js
    lll4
    l2

--- vite.config.js
+++ vite.config.js
@@ -24,40 +24,32 @@
 
 
 
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-
-// export default defineConfig({
-//   plugins: [vue()],
-//   resolve: {
-//     alias: {
-//       '@canvasjs/vue-charts': '/node_modules/@canvasjs/vue-charts/dist/index.js',
-//     },
-//   },
-//   build: {
-//     rollupOptions: {
-//        external: ['@canvasjs/vue-charts'],
-//        output:{
-//         manualChunks(id){
-//           if(id.includes("node_modules")){
-//             return id.toString().split('node_modules')[1].split('/').toString()
-//           }
-//         }
-//       }
-//     },
-//   },
-// })
-
-
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@canvasjs/vue-charts': '/node_modules/@canvasjs/vue-charts/dist/index.js',
+    },
+  },
+  build: {
+    rollupOptions: {
+       external: ['@canvasjs/vue-charts'],
+       output:{
+        manualChunks(id){
+          if(id.includes("node_modules")){
+            return id.toString().split('node_modules')[1].split('/').toString()
+          }
+        }
+      }
+    },
+  },
 })
 
 
+ 
 
 
 
