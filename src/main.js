58694efbc6c3b9b9e6e014c645320231a8d45a88
Mavlonberdiyev Xudoyberdi <129485330+urgut200404@users.noreply.github.com>
2025-01-09 12:56:54 +0300

    Update main.js
    main.js update....)

--- src/main.js
+++ src/main.js
@@ -1,15 +1,31 @@
+// import { createApp } from 'vue' 
+// import './style.css'
+// import App from './App.vue'
+// import router from './Router/index.js'
+// import naive from "naive-ui" 
+
+// import CanvasJSChart from '@canvasjs/vue-charts';
+// const app = createApp(App)
+
+// app.use(router)
+// app.use(naive)  
+// app.use(CanvasJSChart);
+// app.mount('#app')
+
+
+
+
 import { createApp } from 'vue' 
 import './style.css'
 import App from './App.vue'
 import router from './Router/index.js'
 import naive from "naive-ui" 
 
-import CanvasJSChart from '@canvasjs/vue-charts';
+import * as CanvasJS from '@canvasjs/vue-charts';
+
 const app = createApp(App)
 
 app.use(router)
 app.use(naive)  
-app.use(CanvasJSChart);
+app.component('CanvasJSChart', CanvasJS.CanvasJSChart); // Komponentni registratsiya qilyapmiz
 app.mount('#app')
-
-
