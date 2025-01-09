b428f7fd39298cb1fd0951f5aba6b686dda325e5
Mavlonberdiyev Xudoyberdi <129485330+urgut200404@users.noreply.github.com>
2025-01-09 13:02:41 +0300

    Update vite.config.js
    aaaaaaa

--- vite.config.js
+++ vite.config.js
@@ -62,11 +62,18 @@ export default defineConfig({
   build: {
     rollupOptions: {
       external: ['@canvasjs/vue-charts'],
+       output:{
+        manualChunks(id){
+          if(id.includes("node_modules")){
+            return id.toString().split('node_modules')[1].split('/').toString()
+          }
+        }
+      }
     },
   },
 })
 
-
+ 
 
 
 
