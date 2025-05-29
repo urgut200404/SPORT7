d4d78145c4f8ce8ce03dd067c353204c716bf0e8
Mavlonberdiyev Xudoyberdi <129485330+urgut200404@users.noreply.github.com>
2025-05-29 21:14:02 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,43 @@
+// import { createApp } from 'vue';
+// import './style.css';
+// import App from './App.vue';
+// import router from './Router/index.js';
+// import naive from "naive-ui"; 
+
+// const app = createApp(App);
+
+// app.use(router);
+// app.use(naive);
+
+// // Global event listenerlar
+// window.addEventListener("touchmove", (event) => {}, { passive: true });
+// window.addEventListener("touchstart", (event) => {}, { passive: true });
+
+// app.mount('#app');
+
+
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import router from './Router/index.js';
-import naive from "naive-ui"; 
+import naive from 'naive-ui';
 
 const app = createApp(App);
+
+router.beforeEach((to, from, next) => {
+  if (to.meta && to.meta.title) {
+    document.title = to.meta.title;
+  } else {
+    document.title = 'JDPU - Jismoniy Madaniyat Kafedrasi';
+  }
+  next();
+});
 
 app.use(router);
 app.use(naive);
 
 // Global event listenerlar
-window.addEventListener("touchmove", (event) => {}, { passive: true });
-window.addEventListener("touchstart", (event) => {}, { passive: true });
+window.addEventListener('touchmove', (event) => {}, { passive: true });
+window.addEventListener('touchstart', (event) => {}, { passive: true });
 
 app.mount('#app');
